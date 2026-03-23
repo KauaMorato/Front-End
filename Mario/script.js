2ad4e131b77b3e87d5c819dfd149e9c4a14fdf46
Kauã Morato <dxrkitslove4@gmail.com>
2026-03-23 01:00:29 -0300

    Sistema de pontos

--- Mario/script.js
+++ Mario/script.js
@@ -1,20 +1,25 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const scoreElement = document.getElementById('score')
+
+let score = 0;
+let alreadyPassedThisPipe = false;
 
 const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500);
-}
+    if (!mario.classList.contains('jumo')) {
+         mario.classList.add('jump');
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+};
 
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > -10 && marioPosition < 100) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -27,8 +32,20 @@ const loop = setInterval(() => {
         mario.style.marginleft = '50px'
 
         clearInterval(loop);
+        return;
+    }
+
+    if (pipePosition < -50 && !alreadyPassedThisPipe) {
+        score += 100;
+        scoreElement.textContent = score;
+        alreadyPassedThisPipe = true;
+    }
+
+    if (pipePosition > 300) {
+        alreadyPassedThisPipe = false;
     }
 
 }, 10);
 
 document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
