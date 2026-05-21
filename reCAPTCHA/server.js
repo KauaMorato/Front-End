83313f175084ada7f56d56bed4d8f940cbe1551f
Kauã Morato <dxrkitslove4@gmail.com>
2026-05-20 21:16:32 -0300

    Terminando a segunda parte

--- reCAPTCHA/server.js
+++ reCAPTCHA/server.js
@@ -4,48 +4,60 @@ const fs = require('fs')
 const request = require("request")
 var people = [];
 
-fs.readFile('InfosProple.json', function read(err, data){
-    if(err){
+fs.readFile('InfosPeople.json', function read(err, data) {
+    if (err) {
         throw err;
     }
-    try{
+    try {
         var peopleInfo = JSON.parse(data);
         people = peopleInfo
-    }catch(e){
+    } catch (e) {
         console.log("Arquivo Vazio");
     }
 })
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: flase}));
-app.use(bodyParser.jason());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     console.log(req);
 })
 
-app.post('/subscribe', (req, res)=>{
-    if(
+app.post('/subscribe', (req, res) => {
+    if (
         req.body.reCaptcha == undefined ||
         req.body.reCaptcha == '' ||
         req.body.reCaptcha == null
-    ){
-        return res.json({"success": false, 'msg': "Preencha o reCaptcha!"})
+    ) {
+        return res.json({ "success": false, 'msg': "Preencha o reCaptcha!" })
     }
     const secretKey = '6LckEfEsAAAAAIvogwvIHEJioxkBwUGkKFyu-V6G';
 
-    const varifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.reCaptcha}&remoteip=${req.connection.remoteAddress}`
+    const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.reCaptcha}&remoteip=${req.connection.remoteAddress}`;
 
-    request(verifyUrl, (err, response, body)=>{
+    request(verifyUrl, (err, response, body) => {
         body = JSON.parse(body);
 
-        if(body.success != undefined && !body.success){
-            return res.json({"sucess": false, "msg": "Verificação Falhou!"})
+        if (body.success != undefined && !body.success) {
+            return res.json({ "sucess": false, "msg": "Verificação Falhou!" })
         }
 
-        return res.json({"success": true, "msg": "Verificação Sucedida!"})
+        return res.json({ "success": true, "msg": "Verificação Sucedida!" })
     })
+
+    var data = req.body;
+
+    people.push(data);
+
+    fs.writeFile('InfosPeople.json', JSON.stringify(people, null, 2), (e) => {
+        console.log(e);
+    })
+})
+
+app.listen(3000, () => {
+    console.log("servidor iniciado na porta 3000!");
 })
